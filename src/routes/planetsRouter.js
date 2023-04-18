3b10ef718a81170f95a6f19c5d13ea1597e780b8
RominaMB <romimolinabravo@gmail.com>
2023-04-18 18:29:18 -0300

    Corrige tipado rutas (planets)

--- src/routes/planetsRouter.js
+++ src/routes/planetsRouter.js
@@ -2,17 +2,17 @@ const { Router } = require("express");
 const {
   getPlanets,
   getPlanetsById,
-  // createPlanets,
-  // updatePlanetsById,
-  // deletePlanetsById,
+  createPlanet,
+  updatePlanetById,
+  deletePlanetById,
 } = require("../controllers/planetsController");
 
 const planetsRouter = Router();
 
 planetsRouter.get("/", getPlanets);
 planetsRouter.get("/:id", getPlanetsById);
-// planetsRouter.post("/", createPlanets);
-// planetsRouter.put("/:id", updatePlanetsById);
-// planetsRouter.delete("/:id", deletePlanetsById);
+planetsRouter.post("/", createPlanet);
+planetsRouter.put("/:id", updatePlanetById);
+planetsRouter.delete("/:id", deletePlanetById);
 
 module.exports = planetsRouter;
