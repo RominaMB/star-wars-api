379e5b2d23b589bef83f8cfcc273373f48e5fdb7
RominaMB <romimolinabravo@gmail.com>
2023-04-19 02:20:23 -0300

    Actualiza configuración y .env

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -22,17 +22,17 @@ const swaggerSpec = swaggerJSDoc(options);
 
 const swaggerDocs = (app, port) => {
   app.use(
-    "/api/v1/docs",
+    "/v1/docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec, {
       customSiteTitle: "Star Wars API | RominaMB",
     })
   );
-  app.get("/api/v1/docs.json", (req, res) => {
+  app.get("/v1/docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.sebd(swaggerSpec);
   });
-  console.log(`Docs are available at http://localhost:${port}/api/v1/docs`);
+  console.log(`Docs are available at http://localhost:${port}/v1/docs`);
 };
 
 module.exports = { swaggerDocs };
