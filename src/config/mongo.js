0980ae88d3d6701f6f6ebf4f35d9f7c4a4d59e9a
RominaMB <romimolinabravo@gmail.com>
2023-04-19 14:08:50 -0300

    Modificaciones finales

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -4,7 +4,6 @@ const loadCollectionsToDB = require("../utils/loadCollectionsToDB");
 
 const dbConnect = () => {
   const DB_URI = process.env.DB_URI;
-  console.log("DB_URI:", DB_URI);
   mongoose
     .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB Atlas"))
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,43 @@ const filmsRouter = require("./filmsRouter");
 const planetsRouter = require("./planetsRouter");
 const starshipsRouter = require("./starshipsRouter");
 
-router.get("/", (req, res) => {
-  res.send("Welcome to my API");
-});
-
 router.use("/films", filmsRouter);
 router.use("/planets", planetsRouter);
 router.use("/starships", starshipsRouter);
+
+router.get("/", (req, res) => {
+  res.send(`
+    <style>
+      body {
+        font-family: sans-serif, Open Sans;
+        background-color: #F5F5F5;
+      }
+      h1 {
+        color: #89bf04;
+        font-size: 48px;
+        text-align: center;
+        margin-top: 50px
+      }
+
+      h4 {
+        color: #333;
+        font-size: 12px;
+        text-align: center;
+        margin: 20px 0;
+      }
+
+      p {
+        color: #333;
+        font-size: 24px;
+        text-align: center;
+        margin: 20px 0;
+      }
+    </style>
+
+    <h1>Welcome to Star Wars API! 🚀 </h1>
+    <p>Check out our documentation <a href="https://star-wars-api.up.railway.app/v1/docs">here</a> for more information.</p>
+    <h4>Developed By RominaMB</h4>
+  `);
+});
 
 module.exports = router;
