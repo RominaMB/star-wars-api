ab17d620fd5597a8a48425176a14edeafaed3eed
RominaMB <romimolinabravo@gmail.com>
2023-04-18 21:43:00 -0300

    Helmet en app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const routes = require("./routes/index.js");
-const morgan = require("morgan");
+// const morgan = require("morgan");
+const helmet = require("helmet");
 
 const server = express();
 
@@ -11,6 +12,8 @@ server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(cors());
+// server.use(morgan());
+server.use(helmet());
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
